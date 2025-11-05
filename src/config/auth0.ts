878a26db24c82bb09c330b3ee061bd573c49dae6
arthurpereira72@gmail.com <arthurpereira72@gmail.com>
2025-11-05 20:45:36 -0300

    feat: implementado redirect URI híbrido para Android e IOS

--- src/config/auth0.ts
+++ src/config/auth0.ts
@@ -1,9 +1,16 @@
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
+import { Platform } from 'react-native';
 
 export const auth0Domain = 'dev-gk5bz75smosenq24.us.auth0.com';
 export const clientId = '5Rhl8bRWw4JKHtexpJUiR7Dqseu4me3G';
-export const redirectUri = AuthSession.makeRedirectUri({ path: 'callback' });
+
+const dynamicRedirectUri = AuthSession.makeRedirectUri({ path: 'callback' });
+const fixedRedirectUri = 'smart.marketin://auth0';
+
+export const redirectUri = __DEV__ && Platform.OS === 'android' 
+  ? dynamicRedirectUri 
+  : fixedRedirectUri;
 
 WebBrowser.maybeCompleteAuthSession();
 
