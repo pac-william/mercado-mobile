9c6871f0eea2f0aa9b7137cdddc4c83c6794c964
Guilherme Risson <guilhermerisson99@gmail.com>
2025-12-05 23:21:55 -0300

    server.ts update

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,2 +1,2 @@
-// export const apiBaseUrl = "https://api.smart-market.store";
-export const apiBaseUrl = "http://10.0.0.180:8080";
+export const apiBaseUrl = "https://api.smart-market.store";
+// export const apiBaseUrl = "http://10.0.0.180:8080";
